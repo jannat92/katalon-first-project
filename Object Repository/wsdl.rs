c8c4910de1c74507200b4cb953a8b621bb7b0d5d
Liza <fardus1992@gmail.com>
2020-05-15 20:59:27 +0600

    liza first project

---
+++ Object Repository/wsdl.rs
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>wsdl</name>
+   <tag></tag>
+   <elementGuidId>83e58025-d91a-4b16-8ea3-0d315097e515</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <restRequestMethod></restRequestMethod>
+   <restUrl></restUrl>
+   <serviceType>SOAP</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod>SOAP</soapRequestMethod>
+   <soapServiceFunction>GetWeatherInformation</soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+</verificationScript>
+   <wsdlAddress>http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL</wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Test Suites/userlist.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>userlist</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5559d1bf-8a95-4082-82f1-1e0211f0ae21</testSuiteGuid>
+   <testCaseLink>
+      <guid>fc2ca3d9-9cac-49c5-a2b8-d4569a33514e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/userlist/allobject</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>110fe986-d88f-4c75-9719-13a732062709</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/userlist/userlist</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8c33dc11-f4f2-4136-b9e2-fbbb7b93c586</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/calculator Testcases/addition</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6c2a9223-97e9-4a8d-bb1e-4b7b1ee84b65</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/calculator Testcases/subtract</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
